8966abfecff7ba200ddccf00d0835ce0b2685993
Renato Takao Matura de Lima <renato.matura@gmail.com>
2022-07-06 11:06:08 -0300

    Finished lesson about formatting values

--- 02-dt-money/src/index.tsx
+++ 02-dt-money/src/index.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          value: 6000,
+          createdAt: new Date(2022, 6, 6)
+        },
+        {
+          id: 2,
+          title: 'Ifood',
+          type: 'withdraw',
+          category: 'Comida',
+          value: 100,
+          createdAt: new Date(2022, 6, 7)
+        },
+      ]
+    })
+  },
+  
   routes() {
     this.namespace = 'api'
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
     })
   }
 })
