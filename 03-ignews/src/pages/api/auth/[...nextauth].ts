bee56374bd5ede08fa0e877f28f6fcf70c1b1007
Renato Takao Matura de Lima <renato.matura@gmail.com>
2022-08-29 23:56:45 -0300

    Stripe checkout

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,7 @@ import GithubProvider from "next-auth/providers/github"
 import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
+  secret: 'emRmYmdzZGZiZw==',
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -20,9 +21,25 @@ export default NextAuth({
       
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              ),
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
   
